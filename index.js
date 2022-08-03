92b566f962f6a4cfdda790bcdc0016ab8c95e38b
alexbotha <alexbotha@hotmail.co.uk>
2022-08-03 10:35:36 +0100

    commit tree 9849bcb8b19635a2c9d60345bce8a517b4ea923c

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Add your code here
+function submitData(name, email){
+  const configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json",
+    },
+    body: JSON.stringify({
+      "name": name,
+      "email": email,
+    }),
+  }
+  
+   return fetch('http://localhost:3000/users', configurationObject)
+  .then(res => res.json())
+  .then(body => addNewContact(body.id))
+  .catch(error => newError(error.id))
+}
+
+function newError(message){
+  document.getElementsByTagName('body').innerHTML = message
+  document.body.append("Unauthorized Access")
+}
+
+function addNewContact(newId){
+  document.getElementsByTagName('body')[0].innerHTML = newId
+  document.body.append()
+
+}
+
+submitData("name", "name@name.com")
